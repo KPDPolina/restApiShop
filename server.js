ba1138718be205a13beca095838b381797fbeebc
KPDPolina <32580986+KPDPolina@users.noreply.github.com>
2020-10-21 14:44:53 +0300

    Установлен eslint

--- server.js
+++ server.js
@@ -1,6 +1,35 @@
 let express = require("express");
 let app = express();
+
+let artists = [
+    {
+        id: 1,
+        name: "one",
+    },
+    {
+        id: 2,
+        name: "two",
+    },
+    {
+        id: 3,
+        name: "three",
+    },
+]
+
 app.get('/', function(req, res) {
     res.send('Hello API');
 })
-app.listen(3012)
+app.get('/artists', function(req, res) {
+    res.send(artists);
+})
+app.get('/artists/:id', function(req, res) {
+    console.log(req.params);
+    let artist
+    res.send('tttttt');
+})
+
+
+
+app.listen(3012, function(){
+    console.log('3012 app started');
+})
