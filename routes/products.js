5064c506803b20abca0ab050d440ff114f62e279
KPDPolina <32580986+KPDPolina@users.noreply.github.com>
2020-10-23 15:10:59 +0300

    Реализована функция изменения элемента
    Переделана функция выбора продукта для изменения.
    Реализована функция изменения данных о продукте.

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,6 @@ const { Router } = require('express')
 const Product = require('../models/Product')  // импортируем сюда модель Product
 const router = Router()
 
-
 //____________________________ ГЛАВНАЯ _______________________________________________
 router.get('/', async (req, res) => {
     const products = await Product.find({}).lean()  //выводим все объекты модели Product
@@ -13,68 +12,7 @@ router.get('/', async (req, res) => {
     })
 })
 
-
-//________________________ СТРАНИЦА СПИСОК ИЗМЕНЕНИЯ ПРОДУКТОВ _______________________
-router.get('/updateList', async (req, res) => {
-  const products = await Product.find({}).lean()  //выводим все объекты модели Product
-
-  res.render('updateList',{
-      title: "Update products",
-      isUpdate: true,
-      products
-  })
-})
-
-
-//________________________ ДЕЙСТВИЕ - ВЫБОР ПРОДУКТА ИЗ СПИСКА ИЗМЕНЕНИЯ ПРОДУКТОВ _______________________
-router.post('/updateList', async (req, res) => {
-  const product = await Product.findById(req.body.id, function (err, docs) { 
-    if (err){ 
-        console.log(err); 
-    } 
-    else{ 
-        console.log("Result : ", docs); 
-    }
-  })
-console.log(product)
-  res.render('update',{
-      title: "Update product",
-      isUpdate: false,
-      product
-  })
-})
-
-
-//________________________ СТРАНИЦА ИЗМЕНЕНИЯ ОДНОГО ПРОДУКТА _______________________
-router.get('/update', async (req, res) => {
-  const products = await Product.find({}).lean()  //выводим все объекты модели Product
-
-  res.render('update',{
-      title: "Update products",
-      isUpdate: true,
-      products
-  })
-})
-
-//________________________ ДЕЙСТВИЕ - ИЗМЕНЕНИЕ ОДНОГО ПРОДУКТА ________!!!!!!________
-router.post('/update', async (req, res) =>{
-//   await Product.findByIdAndDelete(req.body.id, function (err, docs) { 
-//     if (err){ 
-//         console.log(err) 
-//     } 
-//     else{ 
-//         console.log("Update : ", docs); 
-//     } 
-// })
-  const products = await Product.find({}).lean()
-  res.render('index',{
-    title: "Products",
-    isIndex: true,
-    products
-})
-})
-
-//________________________ СТРАНИЦА УДАЛЕНИЯ ОДНОГО ПРОДУКТА _______________________
+//________________________ СТРАНИЦА УДАЛЕНИЯ ПРОДУКТА _______________________
 router.get('/delete', async (req, res) => {
   const products = await Product.find({}).lean()  //выводим все объекты модели Product
   res.render('delete',{
@@ -84,7 +22,7 @@ router.get('/delete', async (req, res) => {
   })
 })
 
-//________________________ ДЕЙСТВИЕ - УДАЛЕНИЕ ОДНОГО ПРОДУКТА _______________________
+//________________________ ДЕЙСТВИЕ - УДАЛЕНИЕ ПРОДУКТА _______________________
 router.post('/delete', async (req, res) =>{
   await Product.findByIdAndDelete(req.body.id, function (err, docs) { 
     if (err){ 
@@ -104,24 +42,76 @@ router.post('/delete', async (req, res) =>{
 
 //________________________ СТРАНИЦА СОЗДАНИЯ ОДНОГО ПРОДУКТА _______________________
 router.get('/create', (req, res) => {
-    res.render('create',{
-        title: "Create product",
-        isCreate: true
-    })
-}) 
+  res.render('create',{
+      title: "Create product",
+      isCreate: true
+  })
+})
 
 //________________________ ДЕЙСТВИЕ - СОЗДАНИЕ ОДНОГО ПРОДУКТА _______________________
 router.post('/create', async (req, res) => {
-    const product = new Product({               //создаем объект с названием product   модели Product
+  const product = new Product({               //создаем объект с названием product   модели Product
 
-      // передаем свойствам модели Product введеные в инпутах формы данные
-        name: req.body.name,            
-        price: req.body.price,
-        category: req.body.category,
-        supplier: req.body.supplier,
-    })
-    await product.save()              // ждем сохранения нового объекта
-    res.redirect('/')
+    // передаем свойствам модели Product введеные в инпутах формы данные
+      name: req.body.name,            
+      price: req.body.price,
+      category: req.body.category,
+      supplier: req.body.supplier,
+  })
+  await product.save()              // ждем сохранения нового объекта
+  res.redirect('/')
+})
+
+//__________________________ СТРАНИЦА СПИСОК ПРОДУКТОВ (ДЛЯ ИЗМЕНЕНИЯ) ________________________
+router.get('/updateList', async (req, res) =>{
+  const products = await Product.find({}).lean()
+
+  res.render('updateList', {
+    title: "Update list",
+    isUpdate: true,
+    products
+  })
+})
+
+//_______________________ ДЕЙСТВИЕ - ВЫБОР ПРОДУКТА (ДЛЯ ИЗМЕНЕНИЯ) _____________________
+router.post('/updateList', async (req, res) =>{
+  const product = await Product.findById(req.body.id, function (err, docs) { 
+    if (err){ 
+        console.log(err) 
+    } 
+    else{ 
+        console.log("Choose : ", docs); 
+    }
+  })
+  res.render('update', {
+    title: "Update product",
+    isUpdate: false,
+    id: product.id,
+    name: product.name,
+    price: product.price,
+    category: product.category,
+    supplier: product.supplier
+  })
+})
+
+//______________________________ДЕЙСТВИЕ - ИЗМЕНЕНИЕ ПРОДУКТА ______________________
+router.post('/update', async (req, res) => {
+  let product = await Product.findByIdAndUpdate(req.body.id, {
+    name: req.body.name,
+    price: req.body.price,
+    category: req.body.category,
+    supplier: req.body.supplier,
+  }, function (err, docs) { 
+    if (err){ 
+        console.log(err) 
+    } 
+    else{ 
+        console.log("Update : ", docs);
+    }
+  })
+product.save()
+const products = Product.find({}).lean()
+res.redirect('/')
 })
 
   module.exports = router
